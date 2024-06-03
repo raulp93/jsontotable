84d11bc33c8efcd5d40879d3cd0f61e5c7c91db3
raulp93 <raulp93@gmail.com>
2024-06-03 13:40:40 -0700

    added logic, main function, as of now it is functional

---
+++ jsontotable.py
@@ -0,0 +1,46 @@
+from tabulate import tabulate
+import json, time
+
+
+
+def create_table(path):
+
+    with open(path, 'r') as infile:
+        score = json.load(infile)
+
+    keys = []
+    values = []
+    for key in score:
+        keys.append(key)
+        values.append(score[key])
+
+    table = tabulate([keys, values], headers="firstrow")
+
+    return table
+
+
+def main():
+
+    print("jsontotable.py is running...")
+
+    while True:
+        time.sleep(0.25)
+
+        with open("request.txt", 'r') as infile:
+            text = infile.read()
+
+        if text == 'send table':
+            print("request recieved")
+            with open("path.txt", "r") as infile:
+                path = infile.read()
+
+            table = create_table(path)
+
+            with open("table.txt", 'w') as outfile:
+                outfile.write(table)
+            print("request fulfilled")
+
+
+
+if __name__ == "__main__":
+    main()
